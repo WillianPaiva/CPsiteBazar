c0e2a23be977e880e542e284d1b78daf0a749fc6
Anthony Cleret <anthony.cleret@orange.fr>
2016-10-03 15:45:28 +0200

    update CTRLAtelier + test

--- src/web/CtrlAtelier.php
+++ src/web/CtrlAtelier.php
@@ -1,32 +1,35 @@
 <?php
-class CtrlAtelie{
+class CtrlAtelier{
 
     var $conn;
 
 
     function __construct($servername, $username, $password, $dbname){
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $this->conn = new mysqli($servername, $username, $password, $dbname);
     }
 
 
     function __destruct(){
-        $this->$conn->close();
+        $this->conn->close();
     }
     //create the connection
 
 
     //check connection
     function checkConnection(){
-        return $this->$conn->connect_error;
+        return $this->conn->connect_error;
     }
 
 
-    function createAtelier($id, $titre, $theme, $date, $remarque, $lieu,
+    function createAtelier($titre, $theme, $date, $remarque, $lieu,
                            $duree, $resume, $capacite, $partenaires , $public_vise, $contenu, $table){
         $sql = "INSERT INTO ".$table." (titre, theme, date, remarque, lieu, duree, resume, capacite, partenaires, public_vise,contenu) ".
-             " VALUES(".$theme.", ".$titre.", ".$date.", ".$remarque.
-             ", ".$lieu.", ".$duree.", ".$resume.", ".$capacite.", ".$partenaires." , ".$public_vise.", ".$contenu.")";
-        return $conn->query($sql);
+             " VALUES ('".$theme."', '".$titre."', '".$date."', '".$remarque.
+             "', '".$lieu."', '".$duree."', '".$resume."', '".$capacite."', '".$partenaires."', '".$public_vise."', '".$contenu."');";
+        if ($this->conn->query($sql) === TRUE){
+            return 'reussi';
+        }
+        return 'failed: '.$sql;
     }
 
 }
